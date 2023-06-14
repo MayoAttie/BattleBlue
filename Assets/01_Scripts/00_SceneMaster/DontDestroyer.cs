5380d36609a0c5ef589be4af1f7934ae19af6f5b
MayoAttie <barobaksa@naver.com>
2023-06-14 12:22:59 +0900

    Remove all files and directories

--- Assets/01_Scripts/00_SceneMaster/DontDestroyer.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-public class DontDestroyer : MonoBehaviour
-{
-    void Start()
-    {
-        DontDestroyOnLoad(this.gameObject);   
-    }
-}
--- Assets/01_Scripts/02_SceneIngame/Observer.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
-public interface Observer
-{
-    void Notify(List<Transform> data);
-    void FindEnemyData(List<Transform> data, GameObject charac);
-}
